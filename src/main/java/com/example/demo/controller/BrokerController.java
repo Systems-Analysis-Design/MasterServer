0561321acfd47c1495d75cebf69b686c80e74f60
raeimh <Raei.mh@gmail.com>
2024-02-15 11:06:50 +0330

    Final Debug

--- src/main/java/com/example/demo/controller/BrokerController.java
+++ src/main/java/com/example/demo/controller/BrokerController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.BrokerJoinRequest;
 import com.example.demo.model.HealthRequestDto;
 import com.example.demo.model.JoinResponse;
 import com.example.demo.service.BrokerService;
+import com.example.demo.metrics.BrokerServerMetrics;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,20 +20,22 @@ import java.util.UUID;
 public class BrokerController {
 
     private final BrokerService brokerService;
+    private final BrokerServerMetrics brokerServerMetrics;
 
     @PostMapping(path = "/api/join")
     public JoinResponse join(@RequestBody BrokerJoinRequest brokerJoinRequest) {
         log.info(String.format("broker joined with address: %s", brokerJoinRequest.host()));
         Broker broker = new Broker(brokerJoinRequest.host(), brokerJoinRequest.host());
+        brokerServerMetrics.incrementBrokerServersCount();
         return brokerService.addBroker(broker);
     }
 
     @PostMapping(path = "/api/health")
     public void health(@RequestBody HealthRequestDto healthRequestDto) {
         log.info(String.format("health request from broker: %s with totalNumberOfMessages: %s, and totalNumberOfQueues: %s",
-                               healthRequestDto.getBrokerName(),
-                               healthRequestDto.getTotalNumberOfMessages(),
-                               healthRequestDto.getTotalNumberOfQueues()));
+                healthRequestDto.getBrokerName(),
+                healthRequestDto.getTotalNumberOfMessages(),
+                healthRequestDto.getTotalNumberOfQueues()));
     }
 
 }
